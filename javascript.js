223aa4fa3660c62bf5fe6940d64ffcb5246b4d8c
chanjieru <chanjieru2003@gmail.com>
2023-06-16 16:31:41 +0800

    Add draw, erase, clear functions

--- javascript.js
+++ javascript.js
@@ -14,18 +14,52 @@ function createGrid(size=16) {
 createGrid();
 
 const cells = document.querySelectorAll('.cell');
-let isDrawing = false;
+let isDrawing = false; //initial values
+let drawClicked = true;
+let eraseClicked = false;
+
 cells.forEach(cell => cell.addEventListener("mousedown", () => {
     isDrawing = true;
-    cell.style.backgroundColor = 'black';
-}));
-cells.forEach(cell => cell.addEventListener("mousemove", () => {
-    if (isDrawing) {
+    if (eraseClicked) {
+        cell.style.backgroundColor = 'white';
+    } else {
         cell.style.backgroundColor = 'black';
     };
 }));
-cells.forEach(cell => cell.addEventListener("mouseup", () => isDrawing = false));
+cells.forEach(cell => cell.addEventListener("mouseover", () => {
+    if (isDrawing && !eraseClicked) {
+        cell.style.backgroundColor = 'black';
+    } else if (isDrawing && eraseClicked) {
+        cell.style.backgroundColor = 'white';
+    };
+}));
+cells.forEach(cell => cell.addEventListener("mouseup", () => {
+    isDrawing = false;
+}));
 
+const draw = document.querySelector('.draw');
+draw.addEventListener('click', () => {
+    drawClicked = true;
+    eraseClicked = false;
+    draw.style.border = '1px solid black';
+    erase.style.border = 'none';
+    clear.style.border = 'none';
+});
+const erase = document.querySelector('.erase');
+erase.addEventListener('click', () => {
+    eraseClicked = true;
+    drawClicked = false;
+    draw.style.border = 'none';
+    erase.style.border = '1px solid black';
+    clear.style.border = 'none';
+});
 
+const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+    cells.forEach(cell => cell.style.backgroundColor = 'white');
+    draw.style.border = 'none';
+    erase.style.border = 'none';
+    clear.style.border = '1px solid black';
+});
 
 
